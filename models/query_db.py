c70b52f2b9306e4b9ead273de279cd149052623f
mrcheng0910 <mrcheng0910@gmail.com>
2015-11-29 08:53:31 +0800

    put the js to the single file

--- models/query_db.py
+++ models/query_db.py
@@ -10,7 +10,7 @@ class DomainWhoisDb(BaseDb):
     def __init__(self):
         BaseDb.__init__(self)  # 执行父类
 
-    def domain_whois(self,domain=""):
+    def domain_whois(self, domain=""):
         """获取域名的whois信息"""
         domain_initial = self.extract_initial(domain)
         sql = " SELECT domain,top_whois_server,reg_email,reg_name,\
@@ -23,5 +23,4 @@ class DomainWhoisDb(BaseDb):
         """提取域名的首字母，用来选取表,主要是针对特殊字母进行处理"""
         initial = domain[0]
         return initial.upper()
-
 
